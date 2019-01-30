c1f190f73a9061aeaab409421e82da1b55bf3ea1
GRIERSON <T7094911@live.tees.ac.uk>
2019-01-30 11:47:39 +0000

    Game Jam Finished

---
+++ Library/Collab/Download/Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public ShapeType shapeType;
+    public Material buttonColour;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        IgnoreShapeType(shapeType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void IgnoreShapeType(ShapeType type)
+    {
+        var shapes = FindObjectsOfType<Shape>();
+
+        foreach (var shape in shapes)
+        {
+            if (shape.Type == type)
+            {
+                Physics.IgnoreCollision(shape.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+            }
+            else
+            {
+                Physics.IgnoreCollision(shape.gameObject.GetComponent<Collider>(), GetComponent<Collider>(), false);
+            }
+        }
+    }
+}
